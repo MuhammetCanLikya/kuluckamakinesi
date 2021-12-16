73b4dd1098c8ead52d2b7700d67ccf68e57ce5fb
MuhammetCanLikya <96202886+MuhammetCanLikya@users.noreply.github.com>
2021-12-16 04:29:41 +0300

    Update Kuluckamakinesi.ino
    timer errors fixed

--- Kuluckamakinesi.ino
+++ Kuluckamakinesi.ino
@@ -1,4 +1,3 @@
-
 #include <dht.h>
 dht DHT;
 //Constants
@@ -9,7 +8,10 @@ dht DHT;
 //Variables
 float hum;  //Stores humidity value
 float temp; //Stores temperature value
-
+int x=0;
+int y=0;
+int a=0;
+int b=0;
 void setup()
 {
     Serial.begin(9600);
@@ -33,23 +35,45 @@ void loop()
     Serial.print(" %, Temp: ");
     Serial.print(temp);
     Serial.println(" Celsius");
-    delay(2000); //Delay 2 sec.
+    delay(1000); //Delay 2 sec.
 
     if (temp > 37) { //benim rölelerimin ışıkları ters çalıştığı için bu şekilde büyüktür yaptım 
+    
     digitalWrite(RELAYPINT, HIGH);
-  } else  {
-    digitalWrite(RELAYPINT, LOW);
+    
+  } 
+  else  {
+       digitalWrite(RELAYPINT, LOW);
   }
     if (hum > 60) {
     digitalWrite(RELAYPINH, HIGH);
   } else  {
     digitalWrite(RELAYPINH, LOW);
   }
-  
- digitalWrite(RELAYPINM, HIGH);
-  delay (3600000); //1 saat boyunca kuluçka motorunu beklet
 
-digitalWrite(RELAYPINM, LOW);
+ if (b==0){
+  digitalWrite(RELAYPINM, HIGH);
+ b++;
+ }
+    if (a==1){
+    if (x==3600){
+ digitalWrite(RELAYPINM, LOW);
+ x=0;
+ a=0;
+ }
+ if (x<=3600)
+ delay (1000);
+ x=x+2;}
 
-delay (18000); //Motorunuz kaç milisaniyede yarım tur yapıyorsa o kadar çalıştırın
+
+
+ if (a==0){
+ if (y==18){
+digitalWrite(RELAYPINM, HIGH);
+y=0;
+a=1;
+}
+if (y<=18){
+delay (1000);
+y=y+2;}}
 }
